0fc09b34054e98e0a14ae266fcd87964d2e833be
kicamSM <kicamSMM@hotmail.com>
2023-11-28 12:59:04 -0700

    guessing game working and passing tests

---
+++ guessing-game.js
@@ -0,0 +1,58 @@
+// write a fuction that accepts a guess (number) 
+// returns a function that allows a user to guess random whole number between 0-99
+// everytime you create a new game a new random num should be selected if guess is less than 
+// return num too high else num too low 
+
+
+function guessingGame() {
+
+    let randNum = Math.floor(Math.random() * 100)
+    console.log("randumNum:", randNum)
+    let count = 1;
+    let won = false;
+
+return function game(guess){
+    console.log("randumNum:", randNum)
+    if(won === true) {
+        return "The game is over, you already won!"
+    } else if(guess > randNum) {
+        count++;
+        console.log("else if guess:", guess )
+        console.log("else if randomNum:", randNum)
+        return `${guess} is too high!`;
+    } else if(randNum > guess){
+        count++;
+        return `${guess} is too low!`;
+    } else {
+        won = true 
+        return `You win! You found ${randNum} in ${count} guesses.`;
+    }
+}
+
+}
+
+
+// let game = guessingGame();
+// let result1 = game(50); // "50 is too low!"
+// let result2 = game(90); // "90 is too high!"
+// let result3 = game(70); // "You win! You found 70 in 3 guesses."
+// let result4 = game(70); // "The game is over, you already won!"
+
+// console.log("result1:", result1)
+// console.log("result2:", result2)
+// console.log("result3:", result3)
+// console.log("result4:", result4)
+
+let game = guessingGame();
+let result1 = game(1); // "50 is too low!"
+let result2 = game(2); // "90 is too high!"
+let result3 = game(3); // "You win! You found 70 in 3 guesses."
+let result4 = game(4); // "The game is over, you already won!"
+
+console.log("result1:", result1)
+console.log("result2:", result2)
+console.log("result3:", result3)
+console.log("result4:", result4)
+
+module.exports = { guessingGame };
+
